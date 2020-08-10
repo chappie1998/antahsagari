7c7dc7e87dca8e5568f3fb479ddf65115148ba4c
Ankit Kumar Saini <35380717+ankitgc1@users.noreply.github.com>
2020-08-10 14:55:26 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+// HIDE NAVBAR ON SCROLL
+
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-56px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("hide1");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function show(e) {
+  if ($(e).html() == 'Show More') {
+    $(e).siblings('div').show('slow');
+    $(e).html('Show Less')
+  }
+  else {
+    $(e).siblings('div').hide('slow');
+    $(e).html('Show More')
+  }
+}
